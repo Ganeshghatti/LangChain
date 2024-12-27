714c554cf288ba197292abf2e9e0820aa1f5deb5
GaneshGhatti <ganeshghatti6@gmail.com>
2024-12-27 22:30:16 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import getpass
+import os
+
+if not os.environ.get("GROQ_API_KEY"):
+  os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
+
+from langchain_groq import ChatGroq
+
+model = ChatGroq(model="llama3-8b-8192")
+
+print(model.invoke("What is the capital of France?"))
